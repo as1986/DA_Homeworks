88cc68669db3a5e29cd039dcd2fc15ef9371e51a
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2014-02-09 15:07:41 -0500

    most likely & unlikely episodes

--- hw1b/calculateLikelihood.py
+++ hw1b/calculateLikelihood.py
@@ -40,13 +40,64 @@ def segment(lines):
     return toReturn
 
 
+def getTransProb(transitions):
+    import math
+    numClasses = len(transitions[0].strip().split())
+    toReturn = []
+    for i in range(numClasses):
+        toAppend = [math.log(float(t.strip().split()[i]))
+                    for t in transitions[:numClasses]]
+        toReturn.append(toAppend)
+    return toReturn
+
+
+def calculateSeqProb(logTable, seq):
+    # print logTable
+    # print len(logTable)
+    toReturn = 0
+    if len(seq) < 2:
+        return toReturn
+    for i in range(1, len(seq)):
+        # print i
+        # print seq[i - 1]
+        # print seq[i]
+        toReturn += logTable[seq[i - 1]][seq[i]]
+    return toReturn / len(seq)
+
+
 def main():
     import sys
     with open(sys.argv[1]) as inputFile:
         inputLines = inputFile.read().split('\n')
         episodes = segment(inputLines)
-        for x in episodes:
-            print getStateTransit(x)
+        # for x in episodes:
+        #     print getStateTransit(x)
+    with open(sys.argv[2]) as transitionFile:
+        transitions = transitionFile.read().split('\n')
+        transProb = getTransProb(transitions)
+        print transProb
+        for idx, x in enumerate(episodes):
+            print \
+                'episode {}, likelihood: {}'.format(
+                    idx, calculateSeqProb(transProb, getStateTransit(x)))
+
+        def printEpisode(episode):
+            for eachUtterance in episode:
+                print ' '.join(eachUtterance[:3])
+                print ' '.join([x.split(':')[0] for x in eachUtterance[4:]])
+
+        # max
+        maxIdx = max(enumerate(episodes), key=lambda x:
+                     calculateSeqProb(transProb, getStateTransit(x[1])))[0]
+        print 'max: episode {}'.format(maxIdx)
+        printEpisode(episodes[maxIdx])
+
+        # min
+        minIdx = min(enumerate(episodes), key=lambda x:
+                     calculateSeqProb(transProb, getStateTransit(x[1])))[0]
+        print 'min: episode {}'.format(minIdx)
+        printEpisode(episodes[minIdx])
+
     return
 
 if __name__ == '__main__':
