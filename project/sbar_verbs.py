685b14024352c655eed97626e6a23cd50db6db2a
Chu-Cheng Lin <chu.cheng.lin@gmail.com>
2014-05-03 19:21:34 -0400

    stem verb

--- project/sbar_verbs.py
+++ project/sbar_verbs.py
@@ -87,10 +87,11 @@ def main():
     for f in files:
         l_name = f[f.rfind('/'):]
         row_num = int(row_num_pat.search(l_name).groups()[0])
+        print row_num
         if f in sbar_presence:
-            rows[row_num].extend(['True'] + list(find_best_and_stem(sbar_presence[f])))
+            rows[row_num] = ['True'] + list(find_best_and_stem(sbar_presence[f]))
         else:
-            rows[row_num].extend(['False'])
+            rows[row_num] = ['False']
 
     for r in rows:
         print r
