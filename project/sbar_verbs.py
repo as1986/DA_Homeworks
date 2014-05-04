d9728591ca1bd9f4a05f5cfc69699a3d335ae3f8
Chu-Cheng Lin <chuchenl@allegro.clab.cs.cmu.edu>
2014-05-03 23:43:45 -0400

    max verb count

--- project/sbar_verbs.py
+++ project/sbar_verbs.py
@@ -74,7 +74,7 @@ def main():
                 print 'row {} already exists: {}'.format(row_num, rows[row_num])
             bests = list(find_best_and_stem(sbar_presence[f]))
             if len(bests) > 0:
-                rows[row_num] = ['True'] + (max(bests, key=lambda x: x[1]))[0]
+                rows[row_num] = ['True', (max(bests, key=lambda x: x[1]))[0] ]
             else:
                 rows[row_num] = ['False', 'NONE']
         else:
